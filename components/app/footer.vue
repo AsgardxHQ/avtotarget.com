6f07bc3113d272ffaf88a580aa11a4c8c22d25c7
Mikhail <asgardxhq@gmail.com>
2023-04-10 15:06:02 +0300

    update

--- components/app/footer.vue
+++ components/app/footer.vue
@@ -1,5 +1,15 @@
 <template>
-  <footer class="h-64 flex justify-center items-center bg-slate-500 mt-8">
-    <span>Footer</span>
+  <footer class="bg-slate-500 mt-8">
+    <div class="w-full py-4 flex item-center justify-center text-xs text-slate-100">
+      <NuxtLink class="py-1 px-2 text-lg hover:text-slate-900" :to="`/${$route.params.locale}`">{{ $t('mainPage') }}</NuxtLink>
+      <NuxtLink class="py-1 px-2 text-lg hover:text-slate-900" :to="`/${$route.params.locale}/pages/about_us`">{{ $t('pages.about_us') }}</NuxtLink>
+      <NuxtLink class="py-1 px-2 text-lg hover:text-slate-900" :to="`/${$route.params.locale}/pages/delivery`">{{ $t('pages.delivery') }}</NuxtLink>
+      <NuxtLink class="py-1 px-2 text-lg hover:text-slate-900" :to="`/${$route.params.locale}/pages/contacts`">{{ $t('pages.contacts') }}</NuxtLink>
+    </div>
+    <div class="w-full flex justify-center text-lg pb-4">&copy; {{ year }} autotarget.com. ALL RIGHTS RESERVED.</div>
   </footer>
 </template>
+
+<script setup lang="ts">
+const year = new Date().getFullYear();
+</script>
--- server/api/export-products.get.ts
+++ server/api/export-products.get.ts
@@ -1,13 +1,65 @@
 import fs from 'fs';
+
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
 export default defineEventHandler(async (event) => {
-  fs.readdir('./temp/products', (err, files) => {
-    if(err) console.log(err);
-    let limit = 0;
-    files.forEach(async (file) => {
-      if(limit >= 10) return;
-      console.log(file);
-      limit++
+  const files = fs.readdirSync('./temp/products');
+  let limit = 0;
+  const obj = {};
+  const arr = []
+  // const countFolder = Math.round(files.length/10000);
+  // for(let i=0;i<countFolder;i++) {
+  //   const dir = `./files/products/${i}`;
+  //   if (!fs.existsSync(dir)){
+  //     fs.mkdirSync(dir, '0744');
+  //   }
+  // }
+  
+  const chunkSize = 10000;
+  for (let i = 0; i < files.length; i += chunkSize) {
+      arr.push(files.slice(i, i + chunkSize));
+      // do whatever
+  }
+  const r = async (currentChunk) => {
+    let inFolder = 10000;
+    arr[currentChunk].forEach(async (file) => {
+      if(limit >= inFolder) {
+        inFolder += inFolder;
+        console.log('change folder', currentChunk);
+      };
+      obj[file.split('.')[0]] = `${currentChunk}/${file}`;
+      const inStr = fs.createReadStream(`./temp/products/${file}`);
+      const outStr = fs.createWriteStream(`./files/products/${currentChunk}/${file}`);
+      inStr.pipe(outStr);
+      limit++;
     });
-  });
+    for(let key in obj) {
+      try {
+        const items = await prisma.items.findMany({
+          where: {
+            code_vendor: key
+          }
+        });
+        if(items.length > 0 && items[0].id) {
+          await prisma.items.update({
+            where: {
+              id: items[0].id
+            },
+            data: {
+              images: [obj[key]]
+            }
+          });
+        } else {
+          console.log(key);
+        }
+      } catch(err) {
+        console.log(err);
+      }
+    }
+    // console.log('END UPDATE IMAGES -> ', currentChunk);
+    return;
+  }
+  // await r(0);
   return { response: 'ok' }
 })
