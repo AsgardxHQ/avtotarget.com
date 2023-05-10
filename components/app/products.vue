015708cff05adaeb63e796d057c6e644117e561b
Mikhail <asgardxhq@gmail.com>
2023-05-10 11:22:29 +0300

    updated products page

--- components/app/products.vue
+++ components/app/products.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="md:flex">
     <div class="sm:w-full md:w-3/12">
-      <filterComponent @changeQuery="changeQuery" class="product-page" />
+      <filterComponent :categories="categories" :filters="filters" @changeQuery="changeQuery" class="product-page" />
     </div>
     <div class="sm:w-full md:w-9/12">
       <div v-if="currentCategory" class="w-full text-xl text-center uppercase">
@@ -28,16 +28,16 @@
   import filterComponent from "@/components/app/filter.vue";
   import Item from "@/components/app/item.vue";
   const route = useRoute();
-  const currentItems = ref([]);
-  const currentCategory = ref([]);
+  const currentItems:any = ref([]);
+  const currentCategory:any = ref({});
   const isLoader = ref(false);
   const pagination = ref({
     count: 0
   });
-  const changeQuery = async (query) => {
+  const changeQuery = async (query:any) => {
     await getData(query);
   };
-  const queryToString = (query) => {
+  const queryToString = (query:any) => {
     let str = ""
     for(let key in query) {
       if(key && query[key]) {
@@ -49,10 +49,19 @@
   
   const getData = async (query = route.query) => {
     const { data, refresh }:any = await useFetch(`/api/v1/items?${queryToString(route.params)}${queryToString(query)}&limit=21`)
-    await refresh({ dedupe: true })
+    // await refresh({ dedupe: true })
     currentItems.value = data.value.items;
     pagination.value.count = +data.value.count;
   }
   await getData();
+
+  const categories:any = ref([]);
+  const filters:any = ref([]);
+  if(categories.value.length === 0 && filters.value.length === 0) {
+    const { data:getCategories }:any = await useFetch(`/api/v1/categories`);
+    const { data:getFilters }:any = await useFetch(`/api/v1/filters`);
+    categories.value = getCategories.value;
+    filters.value = getFilters.value;
+  }
 
 </script>
